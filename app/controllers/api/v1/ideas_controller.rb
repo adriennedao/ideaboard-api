5e3a52c95af359b6dea53fc0333aeca3c6ede30f
AdrienneD <adriennedao@gmail.com>
2017-08-21 23:38:26 -0400

    initial commit

---
+++ app/controllers/api/v1/ideas_controller.rb
@@ -0,0 +1,19 @@
+module Api::V1
+  class IdeasController < ApplicationController
+    def index
+      @ideas = Idea.order("created_at DESC")
+      render json: @ideas
+    end
+
+    def create
+      @idea = Idea.create(idea_params)
+      render jsaon: @idea
+    end
+
+    private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body)
+    end
+  end
+end
